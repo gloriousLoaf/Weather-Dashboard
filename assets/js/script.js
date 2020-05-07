a7f983d359f79c46185294f27f86804c8d7be6e8
David Metcalf <davidmetcalfwork@gmail.com>
2020-05-07 14:46:55 -0700

    Code complete and considerably drier. Loops cleaned up my jQuery in AJAX calls. Added a Clear History button, used CSS to ensure that data is displayed properl capitalized regardless of user's input. Just needs a little tidy up and a readme.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,22 +9,22 @@ $(document).ready(function () {
 
     // fillFromStorage fills sidebar with anthything in localStorage
     function fillFromStorage() {
-        // grab data, parse and push into searchHistory[], see                  line 176??
+        // grab data, parse and push into searchHistory[], see line 206
         searchHistory = localStorage.getItem("cities", JSON.stringify(searchHistory));
         searchHistory = JSON.parse(searchHistory);
         // iterate through searchHistory, displaying in HTML
-        for (i = 0; i <= searchHistory.length; i++) {
+        for (i = 0; i <= searchHistory.length - 1; i++) {
             $("#Recent-" + i).text(searchHistory[i]);
         }
 
         // with sidebar populated, use highest index in searchHistory
         // to display weather from last city typed into search bar
-        // similar method to searchPast(), see line                              198?
+        // similar method to searchPast(), see line 243
         // var to hold last index value
         let lastIndex = (searchHistory.length - 1);
         // concat a jQuery selector & click listener that calls searchPast()
         $("#Recent-" + lastIndex).on("click", searchPast);
-        // .trigger() method that creates a click on that #Recent-x
+        // .trigger() method that 'clicks' on that #Recent-x
         $("#Recent-" + lastIndex).trigger("click");
     }
 });
@@ -39,6 +39,12 @@ $("#city-date").text($cityDate);
 let $clicked = $(".search-button");
 $clicked.on("click", searchCity);
 $clicked.on("click", searchSave);
+// add Enter key for searching as well
+$("input").keyup(function () {
+    if (event.key === "Enter") {
+        $clicked.click();
+    }
+})
 
 // searchCity() fills the Main View sections - #current-view card & #five-day cards
 function searchCity() {
@@ -46,15 +52,16 @@ function searchCity() {
     let $city = (($(this).parent()).siblings("#city-search")).val();
 
     // empty search bar with setTimeout()
-    // we're also capturing this value in searchSave()                                  line xxx
+    // we're also capturing this value in searchSave() line 209
     // so we need it to not clear so fast that it doesn't get captured there
     function clear() {
         $("#city-search").val("");
     }
-    setTimeout(clear, 600);
+    setTimeout(clear, 300);
 
     /* Query for Current Weather (#current-view) */
-    let firstQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + $city + "&units=imperial&appid=c3632a824cb9d8b82f74d0ec35c2639b";
+    let firstQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
+        $city + "&units=imperial&appid=c3632a824cb9d8b82f74d0ec35c2639b";
     $.ajax({
         url: firstQueryURL,
         method: "GET"
@@ -80,7 +87,9 @@ function searchCity() {
         let lon = response.coord.lon;
 
         /* Query for One Call API - this will give us our info for 5 Day Forecast cards */
-        let secondQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly&units=imperial&appid=c3632a824cb9d8b82f74d0ec35c2639b";
+        let secondQueryURL =
+            "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon +
+            "&exclude=hourly&units=imperial&appid=c3632a824cb9d8b82f74d0ec35c2639b";
         $.ajax({
             url: secondQueryURL,
             method: "GET"
@@ -94,115 +103,106 @@ function searchCity() {
             // var for displaying in html & grabbing the right color class
             let $uvIndex = $("#uv-index");
             $uvIndex.text($uv);
+            $uvIndex.blur();
             // if conditionals to add / remove btn classes, changing color
+            // originally one line like $uvIndex.addClass().removeClass() but just too long
             if ($uv <= 2) {
-                $uvIndex.addClass("btn-success").removeClass("btn-warning btn-hazard btn-danger");
+                $uvIndex.addClass("btn-success");
+                $uvIndex.removeClass("btn-warning btn-hazard btn-danger btn-climate-change");
             }
             else if ($uv <= 5) {
-                $uvIndex.addClass("btn-warning").removeClass("btn-success btn-hazard btn-danger");
+                $uvIndex.addClass("btn-warning");
+                $uvIndex.removeClass("btn-success btn-hazard btn-danger btn-climate-change");
             }
             // .btn-hazard is a custom class, riffing on Bootsrap, see style.css
             else if ($uv <= 7) {
-                $uvIndex.addClass("btn-hazard").removeClass("btn-success btn-warning btn-danger");
+                $uvIndex.addClass("btn-hazard");
+                $uvIndex.removeClass("btn-success btn-warning btn-danger btn-climate-change");
             }
-            else if ($uv <= 10) {
-                $uvIndex.addClass("btn-danger").removeClass("btn-success btn-warning btn-hazard");
+            else if ($uv <= 10.99) {
+                $uvIndex.addClass("btn-danger");
+                $uvIndex.removeClass("btn-success btn-warning btn-hazard btn-climate-change");
+            }
+            // .btn-climate-change, like .btn-hazard, is custom
+            // and it's funny because it is sad :(
+            else if ($uv >= 11) {
+                $uvIndex.addClass("btn-climate-change");
+                $uvIndex.removeClass("btn-success btn-warning btn-hazard btn-danger");
             }
 
-            /* THe NEXT 90 LINES LOOK CRAZY! for-loops to clean it up some?
-            see loop in searchDisplay() var */
+            // Date Assignment - convert UNIX response to human readable
+            // array to hold timestamps
+            let days = [];
+            // get UNIX dt from response, skipping [0] as it is current day
+            for (i = 1; i < 6; i++) {
+                days[i] = response.daily[i].dt;
+            }
+            // filter out that empty index 0, thanks to Dominik
+            // https://stackoverflow.com/users/9251185/dominik
+            days = days.filter(item => item);
+            // convert, extract, display:
+            for (i = 0; i < days.length; i++) {
+                // first convert each index to moment Obj w/ .unix()
+                days[i] = moment.unix(days[i]);
+                // then extract just the MM/DD/YYYY human readable date
+                days[i] = days[i].format("l");
+                // display dates in HTML
+                $("#day-" + i).text(days[i]);
+            }
+            console.log(days);
 
-            // Date Assignment - converting UNIX times in response 
-            // to human readable w/ moment.js
-            // get UNIX dt from response
-            let $day1 = response.daily[1].dt;
-            // convert to moment Obj
-            $day1 = moment.unix($day1);
-            // extract just the MM/DD/YYYY human readable date
-            $day1 = $day1.format("l");
-            // repeat for the rest of the 5 Day cards
-            let $day2 = response.daily[2].dt;
-            $day2 = moment.unix($day2);
-            $day2 = $day2.format("l");
-            let $day3 = response.daily[3].dt;
-            $day3 = moment.unix($day3);
-            $day3 = $day3.format("l");
-            let $day4 = response.daily[4].dt;
-            $day4 = moment.unix($day4);
-            $day4 = $day4.format("l");
-            let $day5 = response.daily[5].dt;
-            $day5 = moment.unix($day5);
-            $day5 = $day5.format("l");
+            // array for highTemps on cards, parsed off decimals
+            let highTemps = [];
+            // same method for skipping and removing current day info as above
+            for (i = 1; i < 6; i++) {
+                highTemps[i] = parseInt(response.daily[i].temp.max) + "°F";
+            }
+            highTemps = highTemps.filter(item => item);
+            // loop through and display
+            for (i = 0; i < highTemps.length; i++) {
+                $("#high" + i).text("High: " + highTemps[i]);
+            }
 
-            // display dates in HTML
-            $("#day-1").text($day1);
-            $("#day-2").text($day2);
-            $("#day-3").text($day3);
-            $("#day-4").text($day4);
-            $("#day-5").text($day5);
+            // same process for lows as with highs
+            let lowTemps = [];
+            for (i = 1; i < 6; i++) {
+                lowTemps[i] = parseInt(response.daily[i].temp.min) + "°F";
+            }
+            lowTemps = lowTemps.filter(item => item);
+            for (i = 0; i < lowTemps.length; i++) {
+                $("#low" + i).text("Low: " + lowTemps[i]);
+            }
 
-            // vars for highTemps on cards
-            let $highTemp1 = parseInt(response.daily[1].temp.max) + "°F";
-            let $highTemp2 = parseInt(response.daily[2].temp.max) + "°F";
-            let $highTemp3 = parseInt(response.daily[3].temp.max) + "°F";
-            let $highTemp4 = parseInt(response.daily[4].temp.max) + "°F";
-            let $highTemp5 = parseInt(response.daily[5].temp.max) + "°F";
-            // display in HTML
-            $("#high1").text("High: " + $highTemp1);
-            $("#high2").text("High: " + $highTemp2);
-            $("#high3").text("High: " + $highTemp3);
-            $("#high4").text("High: " + $highTemp4);
-            $("#high5").text("High: " + $highTemp5);
+            // and again for humidity
+            let hums = [];
+            for (i = 1; i < 6; i++) {
+                hums[i] = response.daily[i].humidity + "%";
+            }
+            hums = hums.filter(item => item);
+            for (i = 0; i < hums.length; i++) {
+                $("#hum" + i).text("Humidity: " + hums[i]);
+            }
 
-            // vars for lowTemps
-            let $lowTemp1 = parseInt(response.daily[1].temp.min) + "°F";
-            let $lowTemp2 = parseInt(response.daily[2].temp.min) + "°F";
-            let $lowTemp3 = parseInt(response.daily[3].temp.min) + "°F";
-            let $lowTemp4 = parseInt(response.daily[4].temp.min) + "°F";
-            let $lowTemp5 = parseInt(response.daily[5].temp.min) + "°F";
-            //display
-            $("#low1").text("Low: " + $lowTemp1);
-            $("#low2").text("Low: " + $lowTemp2);
-            $("#low3").text("Low: " + $lowTemp3);
-            $("#low4").text("Low: " + $lowTemp4);
-            $("#low5").text("Low: " + $lowTemp5);
-
-            // vars for humidity
-            let $hum1 = response.daily[1].humidity + "%";
-            let $hum2 = response.daily[2].humidity + "%";
-            let $hum3 = response.daily[3].humidity + "%";
-            let $hum4 = response.daily[4].humidity + "%";
-            let $hum5 = response.daily[5].humidity + "%";
-            // display
-            $("#hum1").text("Humidity: " + $hum1);
-            $("#hum2").text("Humidity: " + $hum2);
-            $("#hum3").text("Humidity: " + $hum3);
-            $("#hum4").text("Humidity: " + $hum4);
-            $("#hum5").text("Humidity: " + $hum5);
-
-            // vars for weather icons
-            let $icon1 = response.daily[1].weather[0].icon;
-            let $icon2 = response.daily[2].weather[0].icon;
-            let $icon3 = response.daily[3].weather[0].icon;
-            let $icon4 = response.daily[4].weather[0].icon;
-            let $icon5 = response.daily[5].weather[0].icon;
-            // get icons from website
-            let $icon1URL = "http://openweathermap.org/img/w/" + $icon1 + ".png";
-            let $icon2URL = "http://openweathermap.org/img/w/" + $icon2 + ".png";
-            let $icon3URL = "http://openweathermap.org/img/w/" + $icon3 + ".png";
-            let $icon4URL = "http://openweathermap.org/img/w/" + $icon4 + ".png";
-            let $icon5URL = "http://openweathermap.org/img/w/" + $icon5 + ".png";
-            // display in HTML
-            $("#icon1").attr({ "src": $icon1URL, "alt": "Day 1 Icon" });
-            $("#icon2").attr({ "src": $icon2URL, "alt": "Day 2 Icon" });
-            $("#icon3").attr({ "src": $icon3URL, "alt": "Day 3 Icon" });
-            $("#icon4").attr({ "src": $icon4URL, "alt": "Day 4 Icon" });
-            $("#icon5").attr({ "src": $icon5URL, "alt": "Day 5 Icon" });
+            // and again for icons, w/ extra step
+            let icons = [];
+            // each icon will need its own concatenated URL
+            let iconsURL = [];
+            for (i = 1; i < 6; i++) {
+                icons[i] = response.daily[i].weather[0].icon;
+            }
+            icons = icons.filter(item => item);
+            // filling iconsURL[] w/ unique URLs using icons[] indices
+            for (i = 0; i < icons.length; i++) {
+                iconsURL[i] = "http://openweathermap.org/img/w/" + icons[i] + ".png";
+            }
+            for (i = 0; i < iconsURL.length; i++) {
+                $("#icon" + i).attr({ "src": iconsURL[i], "alt": "Daily Weather Icon" });
+            }
         });
     });
 }
 
-// array to use in searchSave() & searchDisplay()
+// array to use in searchSave(), searchDisplay() & clearHistory()
 let searchHistory = [];
 
 // searchSave() uses localStorage to manage recently searched cities in sidebar
@@ -223,15 +223,20 @@ function searchSave() {
 // called by searchSave() after click listener, adds cities to sidebar
 function searchDisplay() {
     // for loop to create new vars & concat them into jQuery selectors
-    for (i = 0; i <= searchHistory.length; i++) {
+    // legit not sure why - 1, just trial and error
+    for (i = 0; i <= searchHistory.length - 1; i++) {
         // iterate through, displaying in HTML
         $("#Recent-" + i).text(searchHistory[i]);
+        // add .past class to create listener (below),
+        // hover effect & styles, see styles.css
+        $("#Recent-" + i).addClass("past");
     }
 }
 
 // add listeners for loading weather from history
-let $pastClick = $(".past");
-$pastClick.on("click", searchPast);
+// wrapped a <section> around search history, since the .past 
+// classes are added after document is fully loaded. bubble up!
+$("section").on("click", ".past", searchPast);
 
 // searchPast() copies text value of the clicked button,
 // puts in the input bar, then runs searchCity() via triggered click
@@ -244,6 +249,24 @@ function searchPast() {
     $clicked.trigger("click");
 }
 
+// Clearing the sidebar
 
-/* CODE GRAVEYARD */
-// empty for now... wait 'til i start trying to loop the ajax stuff
+// clearHistory clears local storage, empties sidebar history
+function clearHistory() {
+    localStorage.clear();
+    // empty main array
+    searchHistory = [];
+    // set first spot to default text & remove .past
+    $("#Recent-0").text("Your Search History").removeClass("past");
+
+    // loop to put reinsert blankspace to keep size correct
+    for (i = 1; i < 7; i++) {
+        $("#Recent-" + i).html("&nbsp;");
+        // remove .past clears listener, hover effect & styling
+        $("#Recent-" + i).removeClass("past");
+    }
+}
+
+// .clear-schedule listener calls clearSchedule()
+let $clear = $("#clear-history");
+$clear.on("click", clearHistory);
